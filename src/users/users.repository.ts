74c8dcfddc8f5db4d41d4bce1b9787480e21e521
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-04-02 16:54:52 -0500

    added more routes

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,11 +1,19 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose"
 import { User, UserDocument } from "./schemas/user.schema";
-import { Model } from "mongoose"
+import { Model, FilterQuery } from "mongoose"
 
 @Injectable()
 export class UsersRepository {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  
+  //can add additional arguments if needed. interface for Users only and allows returning data.
+  async findOne(userFilterQuery: FilterQuery<User>): Promise<User> {
+    return this.userModel.findOne(userFilterQuery)
+  }
+  //this will return an array of users
+  async find(usersFilterQuery: FilterQuery<User>): Promise<User[]> {
+    return this.userModel.find(usersFilterQuery)
+  }
+
 }
