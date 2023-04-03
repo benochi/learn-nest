bbb4fbb071c079e2acd5285793d886b2f3b3179f
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-04-02 19:32:52 -0500

    update users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,14 @@
 import { MongooseModule } from "@nestjs/mongoose";
 import { User, UserSchema } from "./schemas/user.schema";
 import { Module } from '@nestjs/common';
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
+import { UsersRepository } from "./users.repository";
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])]
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository]
 })
 
 export class UsersModule {}
