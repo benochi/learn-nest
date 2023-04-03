dbf39be582fd97262b0261a597ac5caa8f4f6f1c
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-04-02 19:18:47 -0500

    added user services

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -15,5 +15,13 @@ export class UsersRepository {
   async find(usersFilterQuery: FilterQuery<User>): Promise<User[]> {
     return this.userModel.find(usersFilterQuery)
   }
-
+  //create user of type User and return user
+  async create(user: User): Promise<User> {
+    const newUser = new this.userModel(user)
+    return newUser.save()
+  }
+  //can also make this code an abstract class and import it for other types(not User)
+  async findOneAndUpdate(userFilterQuery: FilterQuery<User>, user: Partial<User>): Promise<User> {
+    return this.userModel.findOneAndUpdate(userFilterQuery, user);
+  }
 }
